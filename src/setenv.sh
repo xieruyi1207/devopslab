a60526ec22e265af21a7ece71da7d93650604618
xieruyi1207 <58618619+xieruyi1207@users.noreply.github.com>
2019-12-07 14:09:52 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
 export GITACCOUNT=xieruyi1207
 
-export MYCLUSTER=
-export KUBECONFIG=
+export MYCLUSTER=tektonknativebeijing13
+export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing13/kube-config-tok04-tektonknativebeijing13.yml
 
-export NAMESPACE=
+export NAMESPACE=ektonknativebeijing13
 export EMAIL=xieruyi@icfcc.com
-export INGRESS=
+export INGRESS=tektonknativebeijing13-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
